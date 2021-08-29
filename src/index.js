b5538dfa8a0a1ca947b3c92b4c7e4c2827e1a59f
Alex Shadrin <alexshd@gmail.com>
2021-08-29 21:11:58 +0300

    cleannig unused

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
+import { Elm } from "./Main.elm";
+import * as serviceWorker from "./serviceWorker";
 
-Elm.Main.init({
-  node: document.getElementById('root')
+var storageKey = "store";
+var flags = localStorage.getItem(storageKey);
+
+var app = Elm.Main.init({
+  node: document.getElementById("root"),
+  flags: flags,
 });
 
+app.ports.storeCache.subscribe(function (val) {
+  if (val === null) {
+    localStorage.removeItem(storageKey);
+  } else {
+    localStorage.setItem(storageKey, JSON.stringify(val));
+  }
+
+  // Report that the new session was stored successfully.
+  setTimeout(function () {
+    app.ports.onStoreChange.send(val);
+  }, 0);
+});
+
+// Whenever localStorage changes in another tab, report it if necessary.
+window.addEventListener(
+  "storage",
+  function (event) {
+    if (event.storageArea === localStorage && event.key === storageKey) {
+      app.ports.onStoreChange.send(event.newValue);
+    }
+  },
+  false
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
